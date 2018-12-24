d7d36d7996713780712e9e9365e5b69d5cd759ed
gaoqiong <qiong.gao@krvision.cn>
2018-12-24 09:57:51 +0800

    module转library和application

---
+++ blebluetooth/src/main/java/cn/krvision/blebluetooth/MainActivity.java
@@ -0,0 +1,20 @@
+package cn.krvision.blebluetooth;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, BluetoothActivity.class));
+            }
+        }, 2000);
+    }
+}
